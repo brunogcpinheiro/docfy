e1196f1031755b578808987a34e560815e398b05
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-04-27 14:18:58 -0300

    Creating error handlers, authenticate middlewares, guest middleware and saving sessions in database

--- config/session.js
+++ config/session.js
@@ -1,5 +1,12 @@
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('../app/models');
+
 module.exports = {
   secret: 'docfy2018rocketseatbruno',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
